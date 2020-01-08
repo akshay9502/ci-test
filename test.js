88523921c46ad6813b8ca58535c5b99e1c7f06f6
Akshay Sharma <akshay@kiot.io>
2020-01-08 15:20:19 +0530

    [Add] trying to throw an error

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 
-if (process.env.test_key === 'my_test_key') {
+if (process.env.test_key === 'my_test_key1') {
     console.log('TESTS ARE SUCCESSFULLY COMPLETED!');
 } else {
     throw 'this is wrong, no env found!';
