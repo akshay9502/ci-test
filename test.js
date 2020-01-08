17e3571ff8bb556f448314c2847e75635237b09d
Akshay Sharma <akshay@kiot.io>
2020-01-08 15:17:26 +0530

    [Add] changed error message

--- test.js
+++ test.js
@@ -2,5 +2,5 @@
 if (process.env.test_key === 'my_test_key') {
     console.log('TESTS ARE SUCCESSFULLY COMPLETED!');
 } else {
-    throw 'this is wrong!';
+    throw 'this is wrong, no env found!';
 }
